c9c752f8354998abbe2b2fe1c787c8bfbfcce972
vickiekknight <vickiekknight@gmail.com>
2022-03-28 17:49:35 -0700

    commit

---
+++ Documents/Robotics/mission-control-main/src/store/camerasSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const cameraNames = ["front", "rear", "upperArm", "microscope"];
+
+const initialState = cameraNames.reduce((state, cameraName) => ({
+  ...state,
+  [cameraName]: {
+    isStreaming: false,
+    frameData: null
+  }
+}), {});
+
+const camerasSlice = createSlice({
+  name: "cameras",
+  initialState,
+  reducers: {
+    openCameraStream(state, action) {
+      const { cameraName } = action.payload;
+      state[cameraName].isStreaming = true;
+    },
+
+    closeCameraStream(state, action) {
+      const { cameraName } = action.payload;
+      state[cameraName].isStreaming = false;
+      state[cameraName].frameData = null;
+    },
+
+    cameraStreamDataReportReceived(state, action) {
+      const { cameraName, frameData } = action.payload;
+      if (state[cameraName].isStreaming)
+        state[cameraName].frameData = frameData;
+    }
+  }
+});
+
+export const {
+  openCameraStream,
+  closeCameraStream,
+  cameraStreamDataReportReceived
+} = camerasSlice.actions;
+
+export const selectAllCameraNames = state => Object.keys(state.cameras);
+export const selectCameraIsStreamming = cameraName => state => state.cameras[cameraName].isStreaming;
+export const selectCameraStreamFrameData = cameraName => state => state.cameras[cameraName].frameData;
+
+export default camerasSlice.reducer;
